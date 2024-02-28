bc67f75443cd74f8679ed5aece93fc157e73cd8d
Rutger Vos <rutgeraldo@gmail.com>
2024-02-28 16:12:09 +0100

    added comments

--- nodelib.js
+++ nodelib.js
@@ -1,7 +1,26 @@
+/*
+This library specifies two OO-classes: one for a phylogenetic tree node, and the other for a tree. Instances of these
+classes are manipulated by evolvelib.js to mutate their trait values and trigger their reproduction and death, thereby
+achieving evolution by natural selection. In addition, instances of these classes are also interrogated by uilib.js,
+in order to draw the tree shape as SVG and the current generation in its landscape using HTML5 canvas.
+*/
+
 // object ID counter
 var idCounter = 1;
 
-// simple node object
+/*
+This node object serves three functions:
+1. it is used to help represent the tree shape and color the branches. As such it needs to be recursive data structure
+   with reference to the parent (in the this.parent field) and the children (the this.children array). In addition it
+   holds a slot for color, which is an array of three elements for the RGB values, and a slot for the generation at
+   which the node was instantiated, which is used to calculate the branch length.
+2. it holds the values with which it is plotted on the landscape. For this it has a value for the circle radius (not
+   really used other than the default value of 10 pixels) and the position, which is a two-element array for X and Y.
+3. it has some of the tooling with which to compute the fitness and to make it participate in the evolutionary process.
+   For this it has the this.fitness field, to which the computed fitness is applied by the evolvelib.
+In addition, the object has a field for the unique ID of the object, which is assigned upon instantiation by
+incrementing the global idCounter. The object can be cloned by passing it into the constructor as an argument.
+*/
 var Node = function (args) {
 
     // copy color or instantiate
@@ -48,6 +67,8 @@ var Node = function (args) {
     if ( args && args.parent ) {
         this.parent = args.parent;
     }
+
+    // assigns unique ID as an int
     this.id = idCounter++;
 
     // initialize fitness
@@ -56,12 +77,19 @@ var Node = function (args) {
     //console.log("Instantiated node");
 };
 
+/*
+Computes the raw fitness value of the node object. The fitness is simply the sum of the absolute differences between
+the object's RGB values and that of the underlying canvas.
+*/
 Node.prototype.computeRawFitness = function(landscape) {
     var bgP = landscape.getPixelValue(this.pos[0],this.pos[1]);
     var phP = [ this.color[0], this.color[1], this.color[2] ];
     return Math.abs(bgP[0]-phP[0]) + Math.abs(bgP[1]-phP[1]) + Math.abs(bgP[2]-phP[2]);
 };
 
+/*
+Setter and getter for the fitness value.
+*/
 Node.prototype.setFitness = function(fitness) {
     this.fitness = fitness;
 };
@@ -70,6 +98,9 @@ Node.prototype.getFitness = function () {
     return this.fitness;
 };
 
+/*
+Depth-first traversal with pre- and post-order processing function slots
+*/
 Node.prototype.traverse = function(preFunc,postFunc) {
     if ( preFunc ) {
         preFunc(this);
@@ -83,12 +114,18 @@ Node.prototype.traverse = function(preFunc,postFunc) {
     }
 };
 
+/*
+Apply function to all the node's children
+*/
 Node.prototype.iterate = function(func) {
     for ( var i = 0; i < this.children.length; i++ ) {
         func(this.children[i]);
     }
 };
 
+/*
+Get all terminal descendants of node
+*/
 Node.prototype.getLeaves = function () {
     var leaves = [];
     this.traverse(function(node){
@@ -99,6 +136,13 @@ Node.prototype.getLeaves = function () {
     return leaves;
 };
 
+/*
+Makes the focal node split into two.
+TODO: the overall logical could be refactored in a number of ways:
+- by breeding tips with each other, requiring (at least) diploid genomes
+- by incorporating mate choice in this, e.g. scaled by fitness
+- but then where to graft the children. Probably onto the fittest parent.
+*/
 Node.prototype.reproduce = function(generation) {
 
     // create first child
@@ -116,14 +160,20 @@ Node.prototype.reproduce = function(generation) {
     return this.children;
 };
 
+/*
+Kills the focal node. Because we don't want unbranched internal nodes in our bifurcating tree, killing a child means
+that its parent, left with only the other child, must also be removed. That other child must then be attached to the
+grandparent.
+*/
 Node.prototype.die = function (tree) {
 
     // only non-root leaves can be extinguished
     if ( this.parent ) {
         var id = this.id;
 
-        // collect the sister species, which must
-        // be re-attached to the grandparent
+        /* lookup the sister, which must be grafted to the grandparent
+        so that the parent can also be extinguished and we avoid
+        unbranched internal nodes */
         var otherChild;
         this.parent.iterate(function(node){
             if ( node.id != id ) {
@@ -131,6 +181,7 @@ Node.prototype.die = function (tree) {
             }
         });
 
+        // there is a grandparent to which we can graft
         if ( this.parent.parent ) {
             var pid = this.parent.id;
 
@@ -143,9 +194,11 @@ Node.prototype.die = function (tree) {
             });
             this.parent.parent.children = [ otherChild, otherSibling ];
             otherChild.parent = this.parent.parent;
-            otherSibling.parent = this.parent.parent;
+            otherSibling.parent = this.parent.parent; // TODO: check if necessary
             return true;
         }
+
+        // there is no grandparent, so the sibling becomes the root
         else {
             this.parent.children = [];
             otherChild.parent = undefined;
@@ -156,6 +209,10 @@ Node.prototype.die = function (tree) {
     return false;
 };
 
+/*
+Returns the length of the lineage. As the simulation proceeds, the generation counter for the extant nodes is updated.
+Hence, the 'length' increases with every generation, so that the tree can be drawn ultrametrically.
+*/
 Node.prototype.getLength = function () {
     if ( this.parent ) {
         return this.gen - this.parent.gen;
@@ -165,6 +222,9 @@ Node.prototype.getLength = function () {
     }
 };
 
+/*
+Finds the root of the tree from the focal node by traversing up until parent.isRoot() is reached.
+*/
 Node.prototype.getRoot = function () {
     if ( this.parent ) {
         var parent = this.parent;
@@ -180,6 +240,9 @@ Node.prototype.getRoot = function () {
     }
 };
 
+/*
+Checks if focal node is root, which it is assumed to be if it has no parent.
+*/
 Node.prototype.isRoot = function () {
     if ( this.parent ) {
         return true;
@@ -189,6 +252,9 @@ Node.prototype.isRoot = function () {
     }
 };
 
+/*
+Check if focal node is a leaf, which it is assumed to be if it has no children.
+*/
 Node.prototype.isLeaf = function () {
     if ( this.children.length == 0 ) {
         return true;
@@ -198,10 +264,16 @@ Node.prototype.isLeaf = function () {
     }
 };
 
+/*
+Looks up the values in the color array and reformats as an rgb(*, *, *) string.
+*/
 Node.prototype.getRGB = function () {
     return 'rgb('+this.color[0]+','+this.color[1]+','+ this.color[2]+')';
 };
 
+/*
+Looks up whichever trait value is requested. This is used to mutate them.
+*/
 Node.prototype.getTraitValue = function (trait) {
     if ( trait == 'radius' ) {
         return this.radius;
@@ -223,7 +295,9 @@ Node.prototype.getTraitValue = function (trait) {
     }
 };
 
-// simple tree object
+/*
+Simple tree object, which simply wraps around a root object.
+*/
 var Tree = function(root) {
     if ( root ) {
         this.root = root;
